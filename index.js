117c780ee789852daa4753c4ec73842e58424ba6
Noaman-Saleem <noaman.saleem@umt.edu.pk>
2022-06-07 18:52:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+// routes
+app.get("/", (req, res) => {
+  //   res.send("<h1>This will be Home Page</h1>");
+  res.send("Home page");
+});
+
+app.get("/contact", (req, res) => {
+  //   res.send("<h1>Contact Us Page</h1>");
+  res.send("Contact page");
+});
+
+app.get("/products", (req, res) => {
+  res.send("Products page", { products });
+});
+
+app.listen(8080, () => {
+  console.log(`Server Started`);
+});
